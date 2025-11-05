a5cb68214eafd2a33e7f62613f999fb6c2d85fe2
Rodrigo Hime <rodrigo.hime@gmail.com>
2025-11-05 14:38:01 -0300

    Integrate Turbo Streams for favorites management

--- Modern-Frontend-Rails-Hotwire-and-React/north_by_seven_app/app/controllers/favorites_controller.rb
+++ Modern-Frontend-Rails-Hotwire-and-React/north_by_seven_app/app/controllers/favorites_controller.rb
@@ -10,14 +10,18 @@ class FavoritesController < ApplicationController
   def index; end
 
   def create
-    Favorite.create(user: current_user, concert_id: params[:concert_id])
-    render(partial: "favorites/list")
+    @favorite = Favorite.create(user: current_user, concert_id: params[:concert_id])
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
-    render(partial: "favorites/list")
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   private def favorite_params
